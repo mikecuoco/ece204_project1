f510fa6f9f7a9e16459eba703e6a2df84235d7d3
mikecuoco <mcuoco@ucsd.edu>
2023-04-10 23:25:12 -0700

    add embed.py

---
+++ embed.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# Created on: Apr 10, 2023 at 9:03:04 PM
+__author__ = 'Michael Cuoco'
+
+import numpy as np
+import pandas as pd
+from anndata import AnnData
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import scale
+
+# import embedding functions
+from sklearn.manifold import TSNE, MDS
+from sklearn.decomposition import PCA
+from umap import UMAP
+
+# read expression data, put into AnnData object
+expr = pd.read_csv('data/TCGA.HNSC.expression.txt', sep='\t', header=0, index_col=[0,1])
+meta = pd.read_csv('data/TCGA.HNSC.metadata.txt', sep='\t', header=0, index_col=0)
+
+# add sample_id to metadata, reindex
+meta = (
+    expr
+    .reset_index()[['patient_id','sample_id']]
+    .join(meta, on='patient_id')
+    .set_index(['patient_id','sample_id'])
+)
+
+# put into Annotated Data object for nice organization
+adata = AnnData(expr, obs=meta, var=expr.columns.to_frame(name='gene'), dtype=np.float32)
+adata.obs_names = expr.index.get_level_values(1)
+adata.var_names = expr.columns
+
+# remove zeros
+adata = adata[:, adata.X.sum(axis=0) > 0]
+
+# log(x+1) transform each value in matrix
+adata.X_normed = np.log1p(adata.X)
+
+# scale only, don't center (PCA centers internally)
+adata.X_scaled = scale(adata.X_normed, with_mean=False, axis = 1)
+
+# scale and center
+adata.X_standardized = scale(adata.X_normed, with_mean=True, axis=1)
+
+# embed data
+# PCA
+print("Running PCA...")
+adata.obsm["pca"] = PCA().fit_transform(adata.X_scaled)
+
+# MDS
+print("Running MDS...")
+adata.obsm["mds"] = MDS(normalized_stress='auto').fit_transform(adata.X_standardized)
+
+# t-SNE
+print("Running t-SNE...")
+adata.obsm["tsne"] = TSNE().fit_transform(adata.X_standardized)
+
+# UMAP
+print("Running UMAP...")
+adata.obsm["umap"] = UMAP().fit_transform(adata.X_standardized)
+
+# save data
+adata.write_h5ad('data/TCGA.HNSC.embedded.h5ad')
